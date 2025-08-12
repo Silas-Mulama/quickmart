f5705e845ca918be4b3832781f81b6d71bce59f7
teamseduquest <team.eduquestm.com@gmail.com>
2025-08-12 11:37:52 +0300

    Initial commit

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from products.views import *
+urlpatterns = [
+    path('',products_lst,name='products-list'),
+    path('products/',products_lst,name='products_list'),
+    path('category/',category_pg,name='category_list'),
+    path('men-clothing/',menclothing,name='clothing_men'),
+    path('men-clothing/',womenclothing,name='clothing_women'),
+    path('sale-items/',saleitem,name='sale_items'),
+    path('new-arrivals/',newarrivals,name='new_arrivals'),
+    path('product_detail/',product_det,name='product_details'),
+
+]
